0fd0c46fd9d544288b3c6358adadc2a7df78018b
parth <contactparth2664@gmail.com>
2024-04-17 16:31:49 +0530

    cors

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ const { json } = require('express');
 
 app.use(express.json());
 
-app.use(cors({
-  origin:process.env.CLIENT_URL,
-  credentials:true
-}));
+app.use(cors());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
