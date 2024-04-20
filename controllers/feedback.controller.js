2efdf849240ca9dcc32e036697f200f9760b1be6
parth <contactparth2664@gmail.com>
2024-04-20 10:18:57 +0530

    Fix validation logic for subjects and questions in feedback.controller.js

--- controllers/feedback.controller.js
+++ controllers/feedback.controller.js
@@ -9,53 +9,50 @@ const feedbackController = {
   submitQuestion: async (req, res) => {
     try {
       const accessToken = req.cookies.accessToken;
-
+  
       if (!accessToken) {
-        return res.status(400).json({ error: "Cookie not found" });
+        return res.status(401).json({ error: "Unauthorized: Access token missing" });
       }
-
+  
       const decodedToken = jwt.verify(
         accessToken,
         process.env.ACCESS_TOKEN_SECRET
       );
+  
       const { subjects, questions } = req.body;
-
+  
       const email = decodedToken.email;
       const semester = decodedToken.currentsem;
       const role = decodedToken.role;
+  
       if ((!email || !semester) && role === "mentor") {
-        return res.status(400).json({ error: "Invalid user or semester" });
+        return res.status(401).json({ error: "Unauthorized: Invalid user or semester" });
       }
       
-      if (!subjects || !questions ||(
-          subjects.length === 5 ||
-          subjects.length === 6 ||
-          subjects.length === 7
-        ) ||
-        questions.length !== ((subjects.length)*10)
+      if (!subjects || !questions || (
+          subjects.length < 5 || subjects.length > 7
+        ) || questions.length !== subjects.length * 10
       ) {
         return res.status(400).json({
-          error:
-            `Invalid number of subjects or questions entered, subjects must be 5,6 or 7 and questions must be ${subjects.length*10}`,
+          error: `Invalid number of subjects or questions entered, subjects must be between 5 and 7, and questions must be ${subjects.length * 10}`,
         });
       }
-
+  
       const feedbackQuestionArr = new feedbackQuestion({
         email: email,
         semester: semester,
         subjects: subjects,
         questions: questions,
       });
-
+  
       await feedbackQuestionArr.save();
-
-      res
-        .status(200)
-        .json({ message: "Feedback questions submitted successfully" });
+  
+      res.status(200).json({ message: "Feedback questions submitted successfully" });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ error: error.message });
     }
   },
+  
   submitFeedback: async (req, res) => {
     try {
       const accessToken = req.cookies.accessToken;
